6b55f4ff0764f1b0af8e40eb7ea05168b0c4fe38
Neal Bharucha <nealbharucha@yahoo.com>
2021-12-19 01:47:21 -0600

    Update our story section & add to calendar & main photo area so text is more visible

--- src/views/Top/Top.jsx
+++ src/views/Top/Top.jsx
@@ -19,23 +19,22 @@ const Top = ({frontmatter}) => {
   const scrollToSection = useSmoothScrollTo(jumpToAnchor);
 
   const renderer = ({days}) => {
-    // if (days <= 0) {
-    //   // Render a completed state
-    //   return (
-    //     // TODO: added a `hidden={true}` to hide the button for now.
-    //     <Button hidden={true} size="xl" variant="primary" className="text-uppercase main-button"
-    //             onClick={scrollToSection}>
-    //       <p className="buttonTitle">Watch the Ceremony</p>
-    //     </Button>
-    //   );
-    // }
+    if (days <= 0) {
+      // Render a completed state
+      return (
+        // TODO: added a `hidden={true}` to hide the button for now.
+        <Button size="xl" variant="primary" className="text-uppercase main-button"
+                onClick={scrollToSection}>
+          <p className="buttonTitle">Watch the Ceremony</p>
+        </Button>
+      );
+    }
     return (
       <Popup
         modal
         trigger={
           <Button size="xl" variant="primary"
-                  className="main-button-position text-uppercase main-button"
-                  onClick={scrollToSection}>
+                  className="main-button-position text-uppercase main-button">
             <p className="buttonTitle">Add to Calendar</p>
           </Button>
         }
@@ -51,7 +50,7 @@ const Top = ({frontmatter}) => {
                 <div>
                   <FontAwesomeIcon icon={["fab", "google"]} className="calIcon"/>
                   <a
-                    href="begin:VCALENDARVERSION:2.0BEGIN:VEVENTURL:http://localhost:8000/DTSTART:20201213T190000ZDTEND:20201213T193000ZSUMMARY:Floyd/Vazquez Wedding CeremonyDESCRIPTION:Please join our virtual ceremony at https://youtu.be/EXOL-3JyXMI and leave a comment to let us know you&apos;re there! See our website for more information: https://caitlinandjustin2020.com/LOCATION:https://youtu.be/EXOL-3JyXMIEND:VEVENTEND:VCALENDAR"
+                    href="data:text/calendar;charset=utf8,BEGIN:VCALENDAR%0AVERSION:2.0%0ABEGIN:VEVENT%0AURL:http://carlsednaoui.github.io/add-to-calendar-buttons/generator/generator.html%0ADTSTART:20221112T230000Z%0ADTEND:20221115T060000Z%0ASUMMARY:Shikha%20and%20Neal%20Wedding%20Events%0ADESCRIPTION:Shikha%20and%20Neal%20overall%20event%20dates%20for%20their%20wedding%20in%20Cancun,%20Mexico!%20More%20information%20found%20on%20the%20website.%0ALOCATION:Cancun,%20Mexico%0AEND:VEVENT%0AEND:VCALENDAR"
                     target="_blank"
                     rel="noreferrer"
                   >
@@ -60,33 +59,20 @@ const Top = ({frontmatter}) => {
                 </div>
                 <div>
                   <a
-                    href="https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20201213T190000Z/20201213T193000Z&location=https%3A%2F%2Fyoutu.be%2FEXOL-3JyXMI&text=Floyd%2FVazquez%20Wedding%20Ceremony&details=Please%20join%20our%20virtual%20ceremony%20at%20https%3A%2F%2Fyoutu.be%2FEXOL-3JyXMI%20and%20leave%20a%20comment%20to%20let%20us%20know%20you%26apos%3Bre%20there!%20See%20our%20website%20for%20more%20information%3A%20https%3A%2F%2Fcaitlinandjustin2020.com%2F"
+                    href="https://www.google.com/calendar/render?action=TEMPLATE&amp;text=Shikha%20and%20Neal%20Wedding%20Events&amp;dates=20221112T230000Z/20221115T060000Z&amp;details=Shikha%20and%20Neal%20overall%20event%20dates%20for%20their%20wedding%20in%20Cancun,%20Mexico!%20More%20information%20found%20on%20the%20website.&amp;location=Cancun,%20Mexico&amp;sprop=&amp;sprop=name:"
                     target="_blank"
                     rel="noreferrer"
                   >Google</a>
                 </div>
                 <div>
                   <a
-                    href="begin:VCALENDARVERSION:2.0BEGIN:VEVENTURL:http://localhost:8000/DTSTART:20201213T190000ZDTEND:20201213T193000ZSUMMARY:Floyd/Vazquez Wedding CeremonyDESCRIPTION:Please join our virtual ceremony at https://youtu.be/EXOL-3JyXMI and leave a comment to let us know you&apos;re there! See our website for more information: https://caitlinandjustin2020.com/LOCATION:https://youtu.be/EXOL-3JyXMIEND:VEVENTEND:VCALENDAR"
-                    target="_blank"
-                    rel="noreferrer"
-                  >Outlook</a>
-                </div>
-                <div>
-                  <a
-                    href="begin:VCALENDARVERSION:2.0BEGIN:VEVENTURL:http://localhost:8000/DTSTART:20201213T190000ZDTEND:20201213T193000ZSUMMARY:Floyd/Vazquez Wedding CeremonyDESCRIPTION:Please join our virtual ceremony at https://youtu.be/EXOL-3JyXMI and leave a comment to let us know you&apos;re there! See our website for more information: https://caitlinandjustin2020.com/LOCATION:https://youtu.be/EXOL-3JyXMIEND:VEVENTEND:VCALENDAR"
-                    target="_blank"
-                    rel="noreferrer"
-                  >Outlook.com</a>
-                </div>
-                <div>
-                  <a
-                    href="https://calendar.yahoo.com/?v=60&view=d&type=20&title=Floyd%2FVazquez%20Wedding%20Ceremony&st=20201213T190000Z&dur=0:30&desc=Please%20join%20our%20virtual%20ceremony%20at%20https%3A%2F%2Fyoutu.be%2FEXOL-3JyXMI%20and%20leave%20a%20comment%20to%20let%20us%20know%20you%26apos%3Bre%20there!%20See%20our%20website%20for%20more%20information%3A%20https%3A%2F%2Fcaitlinandjustin2020.com%2F&in_loc=https%3A%2F%2Fyoutu.be%2FEXOL-3JyXMI"
+                    href="https://calendar.yahoo.com/?v=60&amp;view=d&amp;type=20&amp;title=Shikha%20and%20Neal%20Wedding%20Events&amp;st=20221112T170000Z&amp;dur=5500&amp;desc=Shikha%20and%20Neal%20overall%20event%20dates%20for%20their%20wedding%20in%20Cancun,%20Mexico!%20More%20information%20found%20on%20the%20website.&amp;in_loc=Cancun,%20Mexico"
                     target="_blank"
                     rel="noreferrer"
                   >Yahoo</a>
                 </div>
               </div>
+
               {/* calendarLinksWrapper */}
             </div>
             {/* calendarContent */}
@@ -99,7 +85,7 @@ const Top = ({frontmatter}) => {
 
   const extraInfoPart = (
     <Countdown
-      date='2020-12-13T14:00:00-05:00'
+      date='2022-11-09T8:00:00-05:00'
       renderer={renderer}
     />
   );
