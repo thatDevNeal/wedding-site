df12bb27856537932a0fcf93be1202b0da4aa4ed
Neal Bharucha <nealbharucha@yahoo.com>
2022-02-18 14:27:17 -0600

    Calendar: fix website link and event info

--- src/views/Top/Top.jsx
+++ src/views/Top/Top.jsx
@@ -50,7 +50,7 @@ const Top = ({frontmatter}) => {
                 <div>
                   <FontAwesomeIcon icon={["fab", "google"]} className="calIcon"/>
                   <a
-                    href="data:text/calendar;charset=utf8,BEGIN:VCALENDAR%0AVERSION:2.0%0ABEGIN:VEVENT%0AURL:https://nealandshikha.netlify.app/%0ADTSTART:20221112T230000Z%0ADTEND:20221115T060000Z%0ASUMMARY:Shikha%20and%20Neal%20Wedding%20Events%0ADESCRIPTION:Shikha%20and%20Neal%20overall%20event%20dates%20for%20their%20wedding%20in%20Cancun,%20Mexico!%20More%20information%20found%20on%20the%20website.%0ALOCATION:Cancun,%20Mexico%0AEND:VEVENT%0AEND:VCALENDAR"
+                    href="data:text/calendar;charset=utf8,BEGIN:VCALENDAR%0AVERSION:2.0%0ABEGIN:VEVENT%0AURL:https://www.shikhaandneal.com/%0ADTSTART:20221112T230000Z%0ADTEND:20221115T060000Z%0ASUMMARY:Shikha%20and%20Neal%20Wedding%20Events%0ADESCRIPTION:Shikha%20and%20Neal%20overall%20event%20dates%20for%20their%20wedding%20in%20Cancun,%20Mexico!%20More%20information%20found%20on%20www.shikhaandneal.com%0ALOCATION:Cancun,%20Mexico%0AEND:VEVENT%0AEND:VCALENDAR"
                     target="_blank"
                     rel="noreferrer"
                   >
@@ -59,14 +59,14 @@ const Top = ({frontmatter}) => {
                 </div>
                 <div>
                   <a
-                    href="https://www.google.com/calendar/render?action=TEMPLATE&amp;text=Shikha%20and%20Neal%20Wedding%20Events&amp;dates=20221112T230000Z/20221115T060000Z&amp;details=Shikha%20and%20Neal%20overall%20event%20dates%20for%20their%20wedding%20in%20Cancun,%20Mexico!%20More%20information%20found%20on%20the%20website.&amp;location=Cancun,%20Mexico&amp;sprop=&amp;sprop=name:"
+                    href="https://www.google.com/calendar/render?action=TEMPLATE&amp;text=Shikha%20and%20Neal%20Wedding%20Events&amp;dates=20221112T230000Z/20221115T060000Z&amp;details=Shikha%20and%20Neal%20overall%20event%20dates%20for%20their%20wedding%20in%20Cancun,%20Mexico!%20More%20information%20found%20on%20the%20www.shikhaandneal.com&amp;location=Cancun,%20Mexico&amp;sprop=&amp;sprop=name:"
                     target="_blank"
                     rel="noreferrer"
                   >Google</a>
                 </div>
                 <div>
                   <a
-                    href="https://calendar.yahoo.com/?v=60&amp;view=d&amp;type=20&amp;title=Shikha%20and%20Neal%20Wedding%20Events&amp;st=20221112T170000Z&amp;dur=5500&amp;desc=Shikha%20and%20Neal%20overall%20event%20dates%20for%20their%20wedding%20in%20Cancun,%20Mexico!%20More%20information%20found%20on%20the%20website.&amp;in_loc=Cancun,%20Mexico"
+                    href="https://calendar.yahoo.com/?v=60&amp;view=d&amp;type=20&amp;title=Shikha%20and%20Neal%20Wedding%20Events&amp;st=20221112T170000Z&amp;dur=5500&amp;desc=Shikha%20and%20Neal%20overall%20event%20dates%20for%20their%20wedding%20in%20Cancun,%20Mexico!%20More%20information%20found%20on%20the%20www.shikhaandneal.com&amp;in_loc=Cancun,%20Mexico"
                     target="_blank"
                     rel="noreferrer"
                   >Yahoo</a>
