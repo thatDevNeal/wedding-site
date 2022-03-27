af7ed176754d046bb409a61466f46eebe177980a
Neal Bharucha <nealbharucha@yahoo.com>
2022-03-27 13:35:17 -0500

    wedding link formatted and wording and all

--- src/views/Sections/Services/Services.jsx
+++ src/views/Sections/Services/Services.jsx
@@ -19,20 +19,24 @@ const Services = ({className, frontmatter}) => {
         <SectionHeader header={rootHeader} subheader={rootSubHeader}/>
       </Row>
       <Row>
-        <div className="message">
+        <div className="message" style={{textAlign: 'justify'}}>
           <p className="paragraph1">
             2020 and 2021 has been challenging, but for us, one good thing that has happened has
             been getting engaged. We know we are living in an uncertain time, but Shikha and Neal
             have planned to get married in Cancun, Mexico!
           </p> {/* paragraph1 */}
           <p className="paragraph2">
-            <b>We will provide a link to book your hotel at the resort and safe transportation from
-              the
-              airport to the resort (round trip) in the near future. <i>Please check here time to
-                time for updates!</i></b> Also, please note that these rooms
-            have
-            a refund policy, but they are reserved until mid-June of 2022. This means there is time
-            for everyone to book their rooms.
+            <b>Bookings <i>MUST</i> be made through the booking link provided here: <a
+              href="https://www.shaadidestinations.com/shikha-and-neal" target="_blank"
+              rel="noopener noreferrer"><u>Shikha and Neal
+              Resort Booking</u></a>.</b> You may <b><i><u>only</u></i></b> attend the wedding
+            events when
+            booking your
+            resort stay through
+            the link we have provided. Please be sure to read the &quot;<i>Payment and Cancellation
+            Policy</i>&rdquo; section within our booking link for more information.
+          </p> {/* paragraph2 */}
+          <p className="paragraph3">
             <b><i>Please be sure your passport is valid for at least 6 months from the date of our
               wedding.
               It is <a
@@ -41,18 +45,18 @@ const Services = ({className, frontmatter}) => {
                 law now</u></a> where your passport must be valid for at least 6 months to travel
               internationally. This means your passport must valid until, at minimum, June
               2023! </i></b>
-          </p> {/* paragraph2 */}
-          <p className="paragraph3">
+          </p> {/* paragraph3 */}
+          <p className="paragraph4">
             We hope to see you soon, whether its at the events or down the road when we can get
             together a little bit easier.
             We look forward to seeing you!
-          </p> {/* paragraph3 */}
-          <p className="paragraph4">
-            <br/>Sending our love,
           </p> {/* paragraph4 */}
           <p className="paragraph5">
-            Shikha & Neal
+            <br/>Sending our love,
           </p> {/* paragraph5 */}
+          <p className="paragraph6">
+            Shikha & Neal
+          </p>
         </div>
         {/* message */}
       </Row>
