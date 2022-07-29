c7e9676eef5bc5fe3b1d3d98df7dc1e6d2f7af7b
Neal Bharucha <nealbharucha@yahoo.com>
2022-07-29 12:39:44 -0500

    remove gifting section

--- src/views/Sections/Team/Team.jsx
+++ src/views/Sections/Team/Team.jsx
@@ -23,17 +23,6 @@ const Team = ({className, frontmatter}) => {
       <Row className="charities">
         <Col lg={8} className="mx-auto text-center">
           <p className="large text-muted">
-            Please know that your presence at our wedding is present enough! We are lucky enough
-            to already have nearly everything we need for this new chapter together. <br/>
-            However, for friends and family who have expressed an interest, we have created a gift
-            registry for your convenience
-            here: <a href="https://www.amazon.com/wedding/share/ShikhaAndNeal
-" target="_blank" rel="noopener noreferrer"><b><u>Our Wedding
-            Registry!</u></b></a>
-            <br/><br/>
-            These are just some ideas, but please feel free to gift whatever you like! We appreciate
-            anything and everything. To us, your journey to celebrate with us is enough of a gift!
-            <br/><br/>
             Please understand that gifts are not necessary and are not expected. Your blessing is
             all we ask for.
           </p>
